e63b2a38976cde19ab0aa9ee56d2ea19fd29e50c
nc619 <nc619@ic.ac.uk>
2021-12-09 16:02:43 +0000

    1.5

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns={"/patients3"},loadOnStartup = 1)
+@WebServlet(urlPatterns={"/patients3","/"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
     resp.setContentType("text/html");
-    resp.getWriter().write("Hello World");
+    resp.getWriter().write(" <b>On a webpage, this sentence would be in bold print.</b>");
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
